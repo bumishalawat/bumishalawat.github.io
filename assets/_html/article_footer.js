92349c71db0a61ac4723e14d4494b7caf6751616
bumishalawat <masyitamaghfiroh@gmail.com>
2019-04-11 10:24:25 +0700

    .

--- assets/_html/article_footer.js
+++ assets/_html/article_footer.js
@@ -2,7 +2,7 @@ document.write('\
 \
   <footer class="py-5 bg-dark">\
     <div class="container">\
-      <p class="m-0 text-center text-white">Copyright &copy; Lutfi Masyita 2019</p>\
+      <p class="m-0 text-center text-white">Copyright &copy; Smasif Bumi Sholawat 2019</p>\
     </div>\
   </footer>\
 \
--- assets/_html/article_nav.js
+++ assets/_html/article_nav.js
@@ -1,7 +1,7 @@
 document.write('\
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">\
     <div class="container">\
-      <a class="navbar-brand" href="#">Lutfi Masyita</a>\
+      <a class="navbar-brand" href="#">Interactive Javanese Cultural Map</a>\
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">\
         <span class="navbar-toggler-icon"></span>\
       </button>\
